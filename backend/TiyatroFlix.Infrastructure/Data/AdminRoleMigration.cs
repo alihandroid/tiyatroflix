e03be60ebc13393f10f6fa71377ebf70d63fbefc
Alihan Çelikcan <alihancelikcan@gmail.com>
2025-08-13 10:42:21 +0300

    chore(backend): run backend formatter

--- backend/TiyatroFlix.Infrastructure/Data/AdminRoleMigration.cs
+++ backend/TiyatroFlix.Infrastructure/Data/AdminRoleMigration.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 using TiyatroFlix.Domain.Entities;
